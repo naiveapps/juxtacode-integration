e99ee3613f716ad5f1ba4b464c70f7529dd68045
Yori <yori@yori.me>
2023-05-12 09:43:41 +1000

    Updated comments for ‘timeout’ parameter

--- Sources/JuxtaCodeIntegration/MergeTool.swift
+++ Sources/JuxtaCodeIntegration/MergeTool.swift
@@ -5,6 +5,7 @@ public struct MergeTool {
   /// Tells JuxtaCode to open the given file in a merge tool (if it's not already open) and returns the result.
   /// - Parameter file: The conflicted file to open.
   /// - Parameter repo: The local repository containing the conflicted file.
+  /// - Parameter timeout: Timeout duration for the merge interaction. Default is 5 hours.
   /// - Returns: Whether or not the user resolved the conflict.
   public static func open(_ file: URL, in repo: URL, timeout: TimeInterval = 18000) async throws -> Result {
     return try await MergeToolClient.open(url: file.resolvingSymlinksInPath(),
